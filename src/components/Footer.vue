4cfca6b62652b7b12958e0c230ba729fa1b79826
Rafael Rodrigues <rafaeltuccio@gmail.com>
2024-08-12 10:23:21 -0300

    firstCommita

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Footer'
+});
+</script>
+
+<template>
+<footer class="footer">
+      <p>&copy; 2024 RPG Character Tracker. Todos os direitos reservados.</p>
+    </footer>
+</template>
+
+<style scoped>
+.footer {
+    width: 100%;
+  background-color: #7159c1;
+  bottom: 0;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+  position: fixed;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,97 @@
+<!-- <script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router'
+
+export default defineComponent({
+  name: 'Header',
+  components: {
+    RouterLink
+  }
+});
+</script>
+
+
+<template>
+    <nav>
+    <RouterLink class="link" to="/">Home</RouterLink>
+    <RouterLink class="link" to="/knights">Knights</RouterLink> 
+    <RouterLink class="link" to="/knights-create">Adicionar</RouterLink>
+  </nav>
+</template>
+<style scoped>
+
+nav {
+  height: 60px;
+  background: #7159c1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+}
+.link {
+  width: 100px;
+  color: #fff;
+  font-size: 18px;
+  cursor: pointer;
+  height: 45px;
+  text-align: center;
+  line-height: 45px;
+  text-decoration: none;
+
+}
+
+</style> -->
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router'
+
+export default defineComponent({
+  name: 'Header',
+  components: {
+    RouterLink
+  }
+});
+</script>
+
+
+<template>
+    <header class="header">
+      <h1>Ficha Técnica RPG</h1>
+      <nav>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/knights">Personagens</router-link></li>
+          <li><router-link to="/knights-create">Criar Personagem</router-link></li>
+        </ul>
+      </nav>
+    </header>
+
+</template>
+<style scoped>
+.header {
+  background-color: #7159c1;
+  color: #fff;
+  padding: 10px;
+  text-align: center;
+}
+
+.header nav ul {
+  list-style: none;
+  padding: 0;
+}
+
+.header nav ul li {
+  display: inline;
+  margin: 0 10px;
+}
+
+.header nav ul li a {
+  color: #fff;
+  text-decoration: none;
+}
+
+
+</style>
+
+
+
